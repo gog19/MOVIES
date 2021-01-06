945a22684be13f4f89efa2a8604c644e6b865be1
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-01-06 20:27:37 +0500

    Create new features for the list

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ var createElement = function (element, elementClass, text) {
     return newElement;
 };
 
-// var newVersionOfArray = movies.map(function (item) {
+// {// var newVersionOfArray = movies.map(function (item) {
 //     return {
 //         categories: item.Categories.split('|'),
 //         title: item.Title.toString(),
@@ -34,18 +34,18 @@ var createElement = function (element, elementClass, text) {
 //     }
 // })
 
-var getSmallImg = function (youtubeId) {
-    return `http://i3.ytimg.com/vi/${youtubeId}/hqdefault.jpg`
-}
+// var getSmallImg = function (youtubeId) {
+//     return `http://i3.ytimg.com/vi/${youtubeId}/hqdefault.jpg`
+// }
 
-var getBigImg = function (youtubeId) {
-    return `http://i3.ytimg.com/vi/${youtubeId}/maxresdefault.jpg`
-}
+// var getBigImg = function (youtubeId) {
+//     return `http://i3.ytimg.com/vi/${youtubeId}/maxresdefault.jpg`
+// }}
 
 
 var elForm = document.querySelector('.movies__form');
 var elInputName = document.querySelector('.movies__name-input');
-var elInputYear = document.querySelector('.movies__year-input');
+var elInputRating = document.querySelector('.movies__rating-input');
 var elMoviesGenre = document.querySelector('.movies__genre');
 var elMoviesSort = document.querySelector('.movies__sort');
 var elMoviesList = document.querySelector('.movies__list');
@@ -64,7 +64,7 @@ for (var movie of movies) {
 
 emptyGenresArray.forEach((genre) => {
     newOption = document.createElement('option');
-
+    newOption.className = 'movies__option'
     newOption.textContent = genre;
 
     elMoviesGenre.appendChild(newOption);
@@ -73,32 +73,47 @@ emptyGenresArray.forEach((genre) => {
 elForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
-
     var moviesSpell = new RegExp(elInputName.value, 'gi');
     var filteredArray = movies.filter(function (film) {
-        return film.title.match(moviesSpell) || film.year === Number(elInputName.value);
-    });
+        return film.title.match(moviesSpell) && film.imdbRating >= Number(elInputRating.value) &&
+            film.categories.some(function (genre) {
+                return genre === elMoviesGenre.value
+            });
+    })
+
+    if (elMoviesSort.value === 'min') {
+        filteredArray.sort(function (first, second) {
+            if (first.imdbRating < second.imdbRating) return -1;
+            if (first.imdbRating > second.imdbRating) return 1;
+            return 0;
+        })
+    }
+    if (elMoviesSort.value === 'max') {
+        filteredArray.sort(function (first, second) {
+            if (first.imdbRating < second.imdbRating) return -1;
+            if (first.imdbRating > second.imdbRating) return 1;
+            return 0;
+        }).reverse();
+    }
+
+    console.log(filteredArray);
 
     elMoviesList.innerHTML = '';
     filteredArray.forEach(function (film) {
         var templateClone = moviesTemplate.cloneNode(true);
 
-
         $_('.movies__img', templateClone).src = film.smallThumbnail;
         $_('.movies__item-title', templateClone).textContent = film.title;
         $_('.movies__item-year', templateClone).textContent = film.year;
         $_('.movies__item-rating', templateClone).textContent = film.imdbRating;
+        $_('.trailer', templateClone).href = `https://www.youtube.com/watch?v=${film.youtubeId}`;
 
         moviesFragment.appendChild(templateClone);
 
     });
 
-
     total.textContent = filteredArray.length
 
     elMoviesList.appendChild(moviesFragment);
-
-
-
 
 }); 
