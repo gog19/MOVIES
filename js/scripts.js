b8c7c4c3888de99e585e7bf693c1e94b44ed23dd
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-01-06 16:10:47 +0500

    Sort new elements

--- js/scripts.js
+++ js/scripts.js
@@ -20,19 +20,27 @@ var createElement = function (element, elementClass, text) {
     return newElement;
 };
 
-var newVersionOfArray = movies.map(function (item) {
-    return {
-        categories: item.Categories,
-        title: item.Title.toString(),
-        imdbId: item.imdb_id,
-        imdbRating: item.imdb_rating,
-        language: item.language,
-        year: item.movie_year,
-        runtime: item.runtime,
-        summary: item.summary,
-        youtubeId: item.ytid
-    }
-})
+// var newVersionOfArray = movies.map(function (item) {
+//     return {
+//         categories: item.Categories.split('|'),
+//         title: item.Title.toString(),
+//         imdbId: item.imdb_id,
+//         imdbRating: item.imdb_rating,
+//         language: item.language,
+//         year: item.movie_year,
+//         runtime: item.runtime,
+//         summary: item.summary,
+//         youtubeId: item.ytid
+//     }
+// })
+
+var getSmallImg = function (youtubeId) {
+    return `http://i3.ytimg.com/vi/${youtubeId}/hqdefault.jpg`
+}
+
+var getBigImg = function (youtubeId) {
+    return `http://i3.ytimg.com/vi/${youtubeId}/maxresdefault.jpg`
+}
 
 
 var elForm = document.querySelector('.movies__form');
@@ -45,48 +53,52 @@ var moviesTemplate = document.querySelector('.movies-template').content;
 var moviesFragment = document.createDocumentFragment();
 var total = document.querySelector('.total');
 
+var emptyGenresArray = [];
+for (var movie of movies) {
+    for (var category of movie.categories) {
+        if (!emptyGenresArray.includes(category)) {
+            emptyGenresArray.push(category);
+        }
+    }
+}
+
+emptyGenresArray.forEach((genre) => {
+    newOption = document.createElement('option');
+
+    newOption.textContent = genre;
+
+    elMoviesGenre.appendChild(newOption);
+});
+
 elForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
-    var filteredArray = newVersionOfArray.filter(function (film) {
-        return film.title.includes(elInputName.value);
+
+    var moviesSpell = new RegExp(elInputName.value, 'gi');
+    var filteredArray = movies.filter(function (film) {
+        return film.title.match(moviesSpell) || film.year === Number(elInputName.value);
     });
-
-    // var moviesSpell = new RegExp(`${elInputName.value}`, 'gi');
-
 
     elMoviesList.innerHTML = '';
     filteredArray.forEach(function (film) {
-        console.log(film.year);
         var templateClone = moviesTemplate.cloneNode(true);
 
-        $_('.movies__img', templateClone).src = `http://i3.ytimg.com/vi/${film.youtubeId}/hqdefault.jpg`;
+
+        $_('.movies__img', templateClone).src = film.smallThumbnail;
         $_('.movies__item-title', templateClone).textContent = film.title;
         $_('.movies__item-year', templateClone).textContent = film.year;
         $_('.movies__item-rating', templateClone).textContent = film.imdbRating;
 
         moviesFragment.appendChild(templateClone);
+
     });
+
 
     total.textContent = filteredArray.length
 
-    // var filteredYear = newVersionOfArray.filter(function (film) {
-    //     return film.year === Number(elInputName.value);
-    // });
-
-    // elMoviesList.innerHTML = '';
-    // filteredYear.forEach(function (film) {
-    //     console.log(film.year);
-    //     var templateClone = moviesTemplate.cloneNode(true);
-
-    //     $_('.movies__img', templateClone).src = `http://i3.ytimg.com/vi/${film.youtubeId}/hqdefault.jpg`;
-    //     $_('.movies__item-title', templateClone).textContent = film.title;
-    //     $_('.movies__item-year', templateClone).textContent = film.year;
-    //     $_('.movies__item-rating', templateClone).textContent = film.imdbRating;
-
-    //     moviesFragment.appendChild(templateClone);
-    // });
-
     elMoviesList.appendChild(moviesFragment);
+
+
+
 
 }); 
